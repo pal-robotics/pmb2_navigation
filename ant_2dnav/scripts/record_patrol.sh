e1fcf9ebd1000a507389654d5f4041c3c49d25d0
enriquefernandez <enrique.fernandez@pal-robotics.com>
2014-12-11 09:09:11 +0100

    Fix output name

--- ant_2dnav/scripts/record_patrol.sh
+++ ant_2dnav/scripts/record_patrol.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rosbag record -o move_base --split --size=1024 /tf /move_base/goal /move_base/NavfnROS/plan /nav_vel /mobile_base_controller/cmd_vel /mobile_base_controller/odom
+rosbag record -o patrol --split --size=1024 /tf /move_base/goal /move_base/NavfnROS/plan /nav_vel /mobile_base_controller/cmd_vel /mobile_base_controller/odom
 
