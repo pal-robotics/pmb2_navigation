0f5654343bb10fef14d63fb42ce0cf4c2e2d0fb6
enriquefernandez <enrique.fernandez@pal-robotics.com>
2014-12-16 19:09:41 +0100

    Add script for filtering odom -> map TF

---
+++ ant_2dnav/scripts/filter_mapping_bag.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $# < 2 ]]
+then
+    echo "Usage: $0 <input bag> <output bag>"
+    exit 1
+fi
+
+INPUT=$1
+OUTPUT=$2
+
+rosbag filter $INPUT $OUTPUT "topic != '/tf' or m.transforms[0].header.frame_id != '/map' and m.transforms[0].child_frame_id != '/odom'"
