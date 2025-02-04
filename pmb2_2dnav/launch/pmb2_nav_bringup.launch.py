c66f902c78ba9f78b5bbbb3d932bf3365de05a59
antoniobrandi <antonio.brandi@pal-robotics.com>
2025-02-04 13:38:41 +0000

    Update pmb2_nav_bringup.launch.py

--- pmb2_2dnav/launch/pmb2_nav_bringup.launch.py
+++ pmb2_2dnav/launch/pmb2_nav_bringup.launch.py
@@ -155,7 +155,6 @@ def private_nav_function(context, *args, **kwargs):
             "navigation.rviz",
         )],
         output="screen",
-        parameters=[{"use_sim_time": True}],
     )
 
     actions.append(laser_bringup_launch)
