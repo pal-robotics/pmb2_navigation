b86021ca4ac9b93c02901c69099134a589cc3b4c
antoniobrandi <antonio.brandi@pal-robotics.com>
2025-05-07 11:22:10 +0200

    fix hokuo

--- pmb2_laser_sensors/launch/laser_sim.launch.py
+++ pmb2_laser_sensors/launch/laser_sim.launch.py
@@ -24,7 +24,7 @@ def generate_launch_description():
 
     pal_laser_filters_node = 'pal_laser_filters'
     dlo_node = 'direct_laser_odometry'
-    lifecycle_manager_node = 'lifecycle_manager_laser_sim'
+    lifecycle_manager_node = 'lifecycle_manager_laser'
 
     pal_laser_filters_config = get_pal_configuration(
         pkg='pal_laser_filters',
